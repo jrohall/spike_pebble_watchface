8222558b3dfcba3ba93fceaa6d39028c22e749d3
rebble <jeremiahrohall@gmail.com>
2023-02-09 23:25:15 +0000

    changes to formatting, cleaned up code

--- src/c/spike_pebble_watchface.c
+++ src/c/spike_pebble_watchface.c
@@ -3,7 +3,6 @@
 Window *window;
 GBitmap *icon_bitmap;
 BitmapLayer *icon_layer;
-//Layer *date_layer;
 TextLayer *clock_text_layer, *day_text_layer, *date_text_layer, *mon_text_layer, *quote_text_layer, *slash;
 
 char date_buffer[4], day_buffer[6], mon_buffer[4];
@@ -35,16 +34,6 @@ void days_tick_handler(struct tm *tii, TimeUnits units_changed){
   text_layer_set_text(mon_text_layer, mon_buffer);
 }
 
-/*
-void mon_tick_handler(struct tm *tii, TimeUnits units_changed){
-  //Here we will be updating the watchface date display
-  //Changing the TextLayer mon_text_layer to show the updated month
-  strftime(mon_buffer, sizeof(mon_buffer), "%m", tii);
-  text_layer_set_text(mon_text_layer, mon_buffer);
-
-}
-*/
-
 void window_load(Window *window){
   //Load bitmaps into GBitmap structures
   //The ID you chose when uploading is prefixed with 'RESOURCE_ID_'
@@ -56,20 +45,6 @@ void window_load(Window *window){
   bitmap_layer_set_bitmap(icon_layer, icon_bitmap);
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(icon_layer));
 
-  //Setting the clock TextLayer
-  clock_text_layer = text_layer_create(GRect(0, 120, 180, 180));
-  text_layer_set_background_color(clock_text_layer, GColorBlack);
-  text_layer_set_text_color(clock_text_layer, GColorWhite);
-  text_layer_set_text_alignment(clock_text_layer, GTextAlignmentCenter);
-  text_layer_set_font(clock_text_layer, fonts_get_system_font(FONT_KEY_LECO_28_LIGHT_NUMBERS));
-   
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(clock_text_layer));
-
-  //Setting the Layer date_layer
-  //date_layer = layer_create(layer_get_bounds(window_get_root_layer(window)));
-  //layer_set_update_proc(date_layer, date_update_proc);
-  //layer_add_child(window_get_root_layer(window), date_layer);
-
   //Setting the quote TextLayer
   quote_text_layer = text_layer_create(GRect(0, 105, 180, 180));
   text_layer_set_background_color(quote_text_layer, GColorClear);
@@ -80,12 +55,19 @@ void window_load(Window *window){
 
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(quote_text_layer));
 
-  
+  //Setting the clock TextLayer
+  clock_text_layer = text_layer_create(GRect(0, 120, 180, 180));
+  text_layer_set_background_color(clock_text_layer, GColorBlack);
+  text_layer_set_text_color(clock_text_layer, GColorWhite);
+  text_layer_set_text_alignment(clock_text_layer, GTextAlignmentCenter);
+  text_layer_set_font(clock_text_layer, fonts_get_system_font(FONT_KEY_LECO_28_LIGHT_NUMBERS));
+   
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(clock_text_layer));
+
   //Setting the day TextLayer
   day_text_layer = text_layer_create(GRect(60, 145, 180, 180));
   text_layer_set_text(day_text_layer, day_buffer);
   text_layer_set_background_color(day_text_layer, GColorClear);
-  //text_layer_set_text_alignment(day_text_layer, GTextAlignmentCenter);
   text_layer_set_text_color(day_text_layer, GColorWhite);
   text_layer_set_font(day_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
 
@@ -96,7 +78,6 @@ void window_load(Window *window){
   date_text_layer = text_layer_create(GRect(105, 145, 180, 180));
   text_layer_set_text(date_text_layer, date_buffer);
   text_layer_set_background_color(date_text_layer, GColorClear);
-  //text_layer_set_text_alignment(date_text_layer, GTextAlignmentCenter);
   text_layer_set_text_color(date_text_layer, GColorWhite);
   text_layer_set_font(date_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
 
@@ -150,9 +131,6 @@ void window_unload(Window *window){
   text_layer_destroy(mon_text_layer);
   text_layer_destroy(date_text_layer);
 
-  //Destroy Layers
-  //layer_destroy(date_layer);
-
 }
 
 void init()
@@ -175,7 +153,6 @@ void init()
   //register the tick function
   tick_timer_service_subscribe(MINUTE_UNIT, (TickHandler) tick_handler);
   tick_timer_service_subscribe(DAY_UNIT, (TickHandler) days_tick_handler);
-  //tick_timer_service_subscribe(MONTH_UNIT, (TickHandler) mon_tick_handler);
 }
  
 void deinit()
