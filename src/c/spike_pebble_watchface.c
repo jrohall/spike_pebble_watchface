231d76d0400d3dcd4c37311e721e04ae2fef32e7
rebble <jeremiahrohall@gmail.com>
2023-02-09 03:24:09 +0000

    added day display and date_tick_timer

--- src/c/spike_pebble_watchface.c
+++ src/c/spike_pebble_watchface.c
@@ -3,7 +3,10 @@
 Window *window;
 GBitmap *icon_bitmap;
 BitmapLayer *icon_layer;
-TextLayer *clock_text_layer, *date_text_layer, *quote_text_layer;
+//Layer *date_layer;
+TextLayer *clock_text_layer, *day_text_layer, *mon_text_layer, *quote_text_layer;
+
+char mon_buffer[4], day_buffer[6];
 char buffer[] = "00:00";
 
 // Tick handler is set to run every minute, this is how we will change the time
@@ -18,13 +21,24 @@ void tick_handler(struct tm *tick_time, TimeUnits units_changed)
   text_layer_set_text(clock_text_layer, buffer);
 }
 
+void date_tick_handler(struct tm *tii, TimeUnits units_changed) {
+  //Here we will be updating the watchface date display
+  //Changing the TextLayer day text to show the updated day
+  strftime(day_buffer, sizeof(day_buffer), "%a", tii);
+  text_layer_set_text(day_text_layer, day_buffer);
+
+  //Changing the TextLayer month text to show the updated month
+  //strftime(mon_buffer, sizeof(mon_buffer), "%d", tii);
+  //text_layer_set_text(mon_text_layer, mon_buffer);
+}
+
 void window_load(Window *window){
   //Load bitmaps into GBitmap structures
   //The ID you chose when uploading is prefixed with 'RESOURCE_ID_'
   icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_SPIKE_CHALK);
    
   //Create BitmapLayers to show GBitmaps and add to Window
-  //Sample images are 127 x 129 pixels
+  //Sample images are 127 x 129 pixels, pebble time round is 180 x 180 pixels
   icon_layer = bitmap_layer_create(GRect(0, 0, 180, 180));
   bitmap_layer_set_bitmap(icon_layer, icon_bitmap);
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(icon_layer));
@@ -37,9 +51,13 @@ void window_load(Window *window){
   text_layer_set_font(clock_text_layer, fonts_get_system_font(FONT_KEY_LECO_28_LIGHT_NUMBERS));
    
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(clock_text_layer));
-  //text_layer_set_text(clock_text_layer, "Whatever happens, happens.");
 
-  //Setting the date TextLayer
+  //Setting the Layer date_layer
+  //date_layer = layer_create(layer_get_bounds(window_get_root_layer(window)));
+  //layer_set_update_proc(date_layer, date_update_proc);
+  //layer_add_child(window_get_root_layer(window), date_layer);
+
+  //Setting the quote TextLayer
   quote_text_layer = text_layer_create(GRect(0, 105, 180, 180));
   text_layer_set_background_color(quote_text_layer, GColorClear);
   text_layer_set_text_color(quote_text_layer, GColorYellow);
@@ -49,6 +67,16 @@ void window_load(Window *window){
 
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(quote_text_layer));
 
+  //Setting the day TextLayer
+  day_text_layer = text_layer_create(GRect(0, 145, 180, 180));
+  text_layer_set_text(day_text_layer, day_buffer);
+  text_layer_set_background_color(day_text_layer, GColorClear);
+  text_layer_set_text_alignment(day_text_layer, GTextAlignmentCenter);
+  text_layer_set_text_color(day_text_layer, GColorWhite);
+  text_layer_set_font(day_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
+
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(day_text_layer));
+
   //Minute Clock Time
   struct tm *t;
   time_t temp;
@@ -57,6 +85,8 @@ void window_load(Window *window){
    
   //Manually call the tick handler when the window is loading
   tick_handler(t, MINUTE_UNIT);
+  date_tick_handler(t, DAY_UNIT);
+
    
 }
  
@@ -70,9 +100,14 @@ void window_unload(Window *window){
   //Destroy TextLayers
   text_layer_destroy(clock_text_layer);
   text_layer_destroy(quote_text_layer);
+  text_layer_destroy(day_text_layer);
+  text_layer_destroy(mon_text_layer);
+
+  //Destroy Layers
+  //layer_destroy(date_layer);
 
 }
- 
+
 void init()
 {
   //Initialize the app elements here!
@@ -81,11 +116,17 @@ void init()
     .load = window_load,
     .unload = window_unload,
   });
+
   //window_set_window_handlers(window, (WindowHandlers) handlers);
   window_stack_push(window, true);
 
+  //initialize the day and month buffers
+  day_buffer[0] = '\0';
+  mon_buffer[0] = '\0';
+
   //register the tick function
   tick_timer_service_subscribe(MINUTE_UNIT, (TickHandler) tick_handler);
+  tick_timer_service_subscribe(DAY_UNIT, (TickHandler) date_tick_handler);
 }
  
 void deinit()
